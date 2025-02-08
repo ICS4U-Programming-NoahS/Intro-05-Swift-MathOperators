2a7c0b80b7878df4c43a9310807580b3118e1bf5
Noah Smith <noah.smith@stu.ocsb.ca>
2025-02-08 17:41:42 +0000

    MathOperators.swift

---
+++ MathOperators.swift
@@ -0,0 +1,36 @@
+//
+// MathOperators.swift
+//
+// Created by Noah Smith
+// Created on 2025-02-07
+// Version 1.0
+// Copyright (c) 2025 Noah Smith. All rights reserved.
+//
+// This program calculates and displays various math operations
+
+// Import foundation library
+import Foundation
+
+let numFloat1: Float = 2
+let numFloat2: Float = 3
+let numDouble1: Double = 2
+let numDouble2: Double = 3
+
+// Addition
+print("12 + 18 = ", (12 + 18))
+// Subtraction
+print("3 - 7 = ", (3 - 7))
+// Multiplication
+print("3 X 14 = ", (3 * 14))
+// Division with integers
+print("6 / 3 = ", (6 / 3))
+// Division with floats
+print("2 / 3 = ", (numFloat1 / numFloat2))
+// Division with doubles
+print("2 / 3 = ", (numDouble1 / numDouble2))
+// Exponent to the power of 2
+print("4^2 = ", pow(4, 2))
+// Exponent to the power of 3
+print("3^3 = ", pow(3, 3))
+// Square root
+print("√16 = ", sqrt(16))
